99c7ed41a485142e6100e57760724f8f455fa0c3
Adriano <adriano_alves_2008@hotmail.com>
2022-06-28 11:00:28 -0300

    adicionando página do faq

--- pages/faq.js
+++ pages/faq.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react'
 import Link from '../src/components/link'
 import Head from 'next/head'
 
+import FAQScreen from '../src/screens/FAQScreen'
+
+export default FAQScreen
+
 export async function getStaticProps() {
   const FAQ_API_URL =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -19,38 +23,38 @@ export async function getStaticProps() {
   }
 }
 
-export default function Faq({ faq }) {
-  // const [faq, setFaq] = useState([])
-  // useEffect(() => {
-  //   const FAQ_API_URL =
-  //     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-  //   fetch(FAQ_API_URL)
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(resposta => {
-  //       setTimeout(() => {
-  //         setFaq(resposta)
-  //       }, 3 * 1000)
-  //     })
-  // }, [])
-  return (
-    <div>
-      <Head>
-        <title>Faq - Alura Cases Campanha</title>
-      </Head>
-      <h1>Alura Cases - Home Page</h1>
-      <Link href="/">Ir para a Home</Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+// export default function Faq({ faq }) {
+//   // const [faq, setFaq] = useState([])
+//   // useEffect(() => {
+//   //   const FAQ_API_URL =
+//   //     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+//   //   fetch(FAQ_API_URL)
+//   //     .then(response => {
+//   //       return response.json()
+//   //     })
+//   //     .then(resposta => {
+//   //       setTimeout(() => {
+//   //         setFaq(resposta)
+//   //       }, 3 * 1000)
+//   //     })
+//   // }, [])
+//   return (
+//     <div>
+//       <Head>
+//         <title>Faq - Alura Cases Campanha</title>
+//       </Head>
+//       <h1>Alura Cases - Home Page</h1>
+//       <Link href="/">Ir para a Home</Link>
+//       <ul>
+//         {faq.map(({ answer, question }) => (
+//           <li key={question}>
+//             <article>
+//               <h2>{question}</h2>
+//               <p>{answer}</p>
+//             </article>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
